3495e7f91d95112b096e389aaf9f5d930e3d9631
Fred Chien <cfsghost@gmail.com>
2014-12-14 08:12:54 +0800

    add example to sort array

---
+++ 2/sort-customize.js
@@ -0,0 +1,16 @@
+var arr = [
+	1,
+	10,
+	2,
+	1000,
+	500,
+	200,
+	5,
+	231
+];
+
+arr.sort(function(a, b) {
+	return a - b;
+});
+
+console.log(arr);
---
+++ 2/sort-string.js
@@ -0,0 +1,13 @@
+var arr = [
+	'Ferd',
+	'Wesley',
+	'Chuck',
+	'Charles',
+	'Frankie',
+	'Stacy',
+	'Rance'
+];
+
+arr.sort();
+
+console.log(arr);
