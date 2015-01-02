eb34ed1caba8fb1e4f277d7f7465cba1bb5b2f57
Fred Chien <cfsghost@gmail.com>
2015-01-02 19:23:25 +0800

    added an example to show failed test script

---
+++ 18/mocha-failed.js
@@ -0,0 +1,10 @@
+describe('Category A', function() {
+
+    it('TestCase-1', function() {
+    });
+
+    it('TestCase-2', function() {
+
+        throw new Error('Something\'s wrong!');
+    });
+});
