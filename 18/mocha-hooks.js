44c285bc0f8c2b93194d9a2ded826c4fe6491968
Fred Chien <cfsghost@gmail.com>
2015-01-02 20:54:39 +0800

    added an example for mocha hooks

---
+++ 18/mocha-hooks.js
@@ -0,0 +1,19 @@
+describe('Category A', function() {
+
+    before(function() {
+        console.log('Doing something before all tests!');
+    });
+
+    beforeEach(function() {
+        console.log('Doing something before each test!');
+    });
+
+    it('TestCase-1', function() {
+    });
+
+    it('TestCase-2', function() {
+    });
+
+    it('TestCase-3', function() {
+    });
+});
