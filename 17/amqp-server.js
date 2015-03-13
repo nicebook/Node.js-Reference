7121c538710853c2fae99e3e1a4805fd50a492ad
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:49:50 +0800

    Add the example to create a RabbitMG server.

---
+++ 17/amqp-server.js
@@ -0,0 +1,11 @@
+﻿var amqp = require('amqp');
+
+var connection = amqp.createConnection({
+    host: 'localhost'
+});
+
+connection.on('ready', function () {
+    connection.queue('task-queue', { autoDelete: false, durable: true }, function(q) {
+        connection.publish('task-queue', 'Hello', { deliveryMode: 2 });
+    });
+});
