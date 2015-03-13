1ed8884d5400fe05ed5acf92b27309a6995f13ab
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:51:01 +0800

    Add the example to add the broadcast function for RabbitMG server.

---
+++ 17/amqp-broadcast.js
@@ -0,0 +1,12 @@
+﻿var amqp = require('amqp');
+
+var connection = amqp.createConnection({
+    host: 'localhost'
+});
+
+connection.on('ready', function () {
+    connection.exchange('broadcast', { type: 'fanout', autoDelete: false },
+        function(exchange) {
+        exchange.publish('', 'Hello');
+    });
+});
