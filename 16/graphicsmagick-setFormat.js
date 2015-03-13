7c4256c082db7d4344e64977ec07ffc726ce385d
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:40:07 +0800

    Add the example to change the image's format via garphicsmagick.

---
+++ 16/graphicsmagick-setFormat.js
@@ -0,0 +1,12 @@
+﻿var gm = require('gm');
+var dirpath = 'images/';
+var imgname = 'download-logo.png';
+var tmppath = 'tmp/';
+
+gm(dirpath + imgname) 
+    .setFormat('gif') 
+    .write(tmppath + 'New_' + imgname, function (err) {
+        if (err)
+            throw err;
+        console.log('done');
+});
