49ea4014d0031a35c8d824c70b0b3d783deb500b
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:33:11 +0800

    Add the example to change format of image via ninja.

---
+++ 16/ninja-toJPG.js
@@ -0,0 +1,13 @@
+﻿var Image = require('image-ninja');
+
+var dirpath = 'images/';
+var imgname = 'download-logo.png';
+var tmppath = 'tmp/'
+
+var image = new Image(dirpath + imgname); 
+image
+	.format('jpg') 
+	.save(tmppath + 'output.jpg')
+	.then(function (newImage) {
+		console.log('Done.'); 
+	});
