481bc3c0196e15b6fe75d4fe057108cd547ab199
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:36:07 +0800

    Add the example to show file size via garphicsmagick.

---
+++ 16/graphicsmagick-filesize.js
@@ -0,0 +1,12 @@
+﻿var gm = require('gm');
+var dirpath = 'images/';
+var imgname = 'download-logo.png';
+var tmppath = 'tmp/';
+
+gm(dirpath + imgname) 
+
+    .filesize(function (err, data) { 
+        if (err)
+            throw err;
+        console.log('done');
+    });
