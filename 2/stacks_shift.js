da2ccf99b7c16f12f7d7477f653374cfe83c2888
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 10:56:57 +0800

    Add the example to shift a element from an array.

---
+++ 2/stacks_shift.js
@@ -0,0 +1,13 @@
+var queue = [];
+
+queue.push('Fred');
+queue.push('Wesley');
+queue.push('Chuck');
+queue.push('Charles');
+queue.push('Frankie');
+
+var first  = queue.shift();
+console.log(first);
+
+var second = queue.shift();
+console.log(second);
