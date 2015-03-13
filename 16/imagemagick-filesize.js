ff9e7f2f4e5e6ba5015082c3c869f00e552167d7
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:36:25 +0800

    Add the example to show file size via imagemagick.

---
+++ 16/imagemagick-filesize.js
@@ -0,0 +1,12 @@
+﻿var gm = require('gm');
+var dirpath = 'images/';
+var imgname = 'download-logo.png';
+var tmppath = 'tmp/';
+var imageMagick = gm.subClass({ imageMagick: true });
+
+imageMagick(dirpath + imgname)  
+    .filesize(function (err, data) { 
+        if (err)
+            throw err;
+        console.log('done');
+    });  
