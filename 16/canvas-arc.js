3c8cbadaae6cb2df4858069930dd81fd1acf4117
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:42:19 +0800

    Add the example to draw a arc via canvas.

---
+++ 16/canvas-arc.js
@@ -0,0 +1,10 @@
+﻿var Canvas = require('canvas');
+var fs = require('fs');
+
+var canvas = new Canvas(640, 480, 'svg');
+var context = canvas.getContext('2d');
+context.beginPath();
+context.arc(100, 100, 50, 0, Math.PI, true);
+context.strokeStyle = '#ff0000';
+context.stroke();
+fs.writeFile('output.svg', canvas.toBuffer());
