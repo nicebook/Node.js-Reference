dc6982ecbd550707f1b831e869a2661752565a70
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:34:11 +0800

    Add the example to rotate image via sharp.

---
+++ 16/sharp-rotate.js
@@ -0,0 +1,13 @@
+﻿var sharp = require('sharp');
+
+var dirpath = 'images/';
+var imgname = 'download-logo.png';
+var tmppath = 'tmp/'
+
+sharp(dirpath + imgname)
+    .rotate(45)
+    .toFile(tmppath + 'New_' + imgname, function (err, info) {
+        if (err) 
+            throw err;
+        console.log('done');
+    });
