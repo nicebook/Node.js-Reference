99ced483ec373f40d83ba8c922a28284558b2bf3
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:54:13 +0800

    Add the example to add the broadcast function for zmq.

---
+++ 17/zmq-broadcast.js
@@ -0,0 +1,9 @@
+﻿var zmq = require('zmq');
+
+var socket = zmq.socket('pub');
+
+socket.bind('tcp://127.0.0.1:2001', function(error) {
+    setInterval(function() {
+        socket.send('ALERT Hello!');
+    }, 1000);
+});
