f9970a0a28613e0ecc5682d2c5f56a1153e6aa69
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:45:37 +0800

    Add the example to import a png file via canvas.

---
+++ 16/canvas-drawImage.js
@@ -0,0 +1,9 @@
+﻿var Canvas = require('canvas');
+var fs = require('fs');
+
+var canvas = new Canvas(640, 480, 'svg');
+var context = canvas.getContext('2d');
+var image = new Canvas.Image();
+image.src = fs.readFileSync('test.png');
+context.drawImage(image, 100, 100, image.width, image.height);
+fs.writeFile('output.svg', canvas.toBuffer());
