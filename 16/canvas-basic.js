441be6596dd0f151ba2f599cd510cc4b755ecf56
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:41:43 +0800

    Add the example to create a svg file via canvas.

---
+++ 16/canvas-basic.js
@@ -0,0 +1,10 @@
+﻿var Canvas = require('canvas');
+var fs = require('fs');
+
+var canvas = new Canvas(640, 480, 'svg');
+var context = canvas.getContext('2d');
+context.beginPath();
+context.rect(0, 0, 640, 480);
+context.fillStyle = '#ff0000';
+context.fill();
+fs.writeFile('output.svg', canvas.toBuffer());
