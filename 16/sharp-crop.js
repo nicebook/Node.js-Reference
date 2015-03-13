780dcdde1a9c6ae437f9aec82de55997b75e2e8c
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:34:23 +0800

    Add the example to crop image via sharp.

---
+++ 16/sharp-crop.js
@@ -0,0 +1,14 @@
+﻿var sharp = require('sharp');
+
+var dirpath = 'images/';
+var imgname = 'download-logo.png';
+var tmppath = 'tmp/'
+
+sharp(dirpath + imgname)
+    .resize(50, 100)
+    .crop()
+    .toFile(tmppath + 'New_' + imgname, function (err, info) {
+        if (err) 
+            throw err;
+        console.log('done');
+    });
