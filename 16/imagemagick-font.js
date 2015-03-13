e820bb9063be321b60c70010002b61d7d291e82b
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:37:23 +0800

    Add the example to write texts in image file via imagemagick.

---
+++ 16/imagemagick-font.js
@@ -0,0 +1,14 @@
+﻿var gm = require('gm');
+var dirpath = 'images/';
+var imgname = 'download-logo.png';
+var tmppath = 'tmp/';
+var imageMagick = gm.subClass({ imageMagick: true });
+
+imageMagick(dirpath + imgname)  
+    .font('fonts/ariali.ttf', 12) 
+    .drawText(5, 10, 'Hello Magic.') 
+    .write(tmppath + 'imageMagick_' + imgname, function (err) {
+        if (err)
+            throw err;
+        console.log('done');
+});
