8ae2f5d1b1e5aa29935ec032147fe730168b0c9a
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:39:39 +0800

    Add the example to show image's info via imagemagick.

---
+++ 16/imagemagick-identify.js
@@ -0,0 +1,12 @@
+﻿var gm = require('gm');
+var dirpath = 'images/';
+var imgname = 'download-logo.png';
+
+var imageMagick = gm.subClass({ imageMagick: true });
+
+imageMagick(dirpath + imgname) 
+    .identify(function(err, data) { 
+    if (err) 
+        throw err;
+    console.log(data);
+    });   
