22f86e3ca7b6ba30b01fe9659970e29bb50c4c4f
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:54:30 +0800

    Add the example to subscribe the broadcast via zmq.

---
+++ 17/zmq-broadcast-subscribe.js
@@ -0,0 +1,9 @@
+﻿var zmq = require('zmq');
+
+var socket = zmq.socket('sub');
+socket.connect('tcp://127.0.0.1:2001');
+
+socket.subscribe('ALERT');
+socket.on('message', function(data) {
+    console.log(data.toString());
+});
