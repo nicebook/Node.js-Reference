4591167a06d69e009c4174cf5025eed1179da963
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:37:51 +0800

    Add the example to crop the image file via garphicsmagick.

---
+++ 16/graphicsmagick-crop.js
@@ -0,0 +1,13 @@
+﻿var gm = require('gm');
+var dirpath = 'images/';
+var imgname = 'download-logo.png';
+var tmppath = 'tmp/';
+
+gm(dirpath + imgname) 
+
+    .crop(50, 100, 5, 10) 
+    .write(tmppath + 'New_' + imgname, function (err) {
+        if (err)
+            throw err;
+        console.log('done');
+});
