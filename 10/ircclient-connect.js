984567a438e8549997f8f79c6b9c4e42334fb27d
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:10:24 +0800

    Add the example to create client to connect to IRC.

---
+++ 10/ircclient-connect.js
@@ -0,0 +1,13 @@
+var irc = require('slate-irc');
+var net = require('net');
+
+var stream = net.connect({
+    port: 6667,
+    host: 'irc.freenode.org'
+});
+
+var client = irc(stream);
+client.pass('pass');
+client.nick('Fred');
+client.user('Fred', 'Fred Chien');
+client.join('#nodejs');
