5ce3fad1a940c1adf36278b8568e2e7b5bcb130a
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:16:42 +0800

    Add the example to send the mail with one attachment.

---
+++ 12/nodemailer-attachment.js
@@ -0,0 +1,32 @@
+var nodemailer = require('nodemailer');
+ 
+var transporter = nodemailer.createTransport({ 
+
+	service: 'Gmail',  
+	auth: {
+		user: 'example@mandice.com',  
+		pass: 'mypasswd' 
+	}
+});
+
+var mailOptions = {
+	from: 'example@mandice.com', 
+	to: 'wesley@mandice.com',   
+	subject: 'nodemailer test', 
+	text: 'Send message from nodemailer!', 
+	html: '<b>Send message from nodemailer!</b>' 
+	attachments: [
+		{	
+			filename: 'text1.txt',
+			content: 'nodemailer test!'
+		}
+	]    	
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+	if(error){
+		console.log(error);
+	}else{
+		console.log('Message sent: ' + info.response);
+	}
+});
