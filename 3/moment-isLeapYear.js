a05884b23124657131d2d0ef0952a557cdf0e9ac
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 11:11:35 +0800

    Add the example to use moment to check the time(LeapYear).

---
+++ 3/moment-isLeapYear.js
@@ -0,0 +1,7 @@
+var moment = require('moment');
+
+console.log(moment('2012-11-15').isLeapYear());
+console.log(moment('2013-11-15').isLeapYear());
+console.log(moment('2014-11-15').isLeapYear());
+console.log(moment('2015-11-15').isLeapYear());
+
