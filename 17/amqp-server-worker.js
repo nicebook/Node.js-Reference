a2f4e0a23386678a46fceaa4efd8802d8dd5d6bd
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:50:21 +0800

    Add the example to the worker for RabbitMG server.

---
+++ 17/amqp-server-worker.js
@@ -0,0 +1,15 @@
+﻿var amqp = require('amqp');
+
+var connection = amqp.createConnection({
+    host: 'localhost'
+});
+
+connection.on('ready', function () {
+    connection.queue('task-queue', { autoDelete: false, durable: true }, function(q) {
+        q.bind('#');
+        q.subscribe({ ack: true, prefetchCount: 1 }, function(message) {
+            console.log(message);
+            q.shift();
+        });
+    });
+});
