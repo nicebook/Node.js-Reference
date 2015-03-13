25c844532eca086465b66651887dbbed821bb979
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:34:36 +0800

    Add the example to resize image via sharp.

---
+++ 16/sharp-resize.js
@@ -0,0 +1,9 @@
+﻿var sharp = require('sharp');
+
+var dirpath = 'images/';
+var imgname = 'download-logo.png';
+var tmppath = 'tmp/'
+
+sharp(dirpath + imgname)
+    .resize(300) 
+    .toFile(tmppath + 'New_' + imgname);
