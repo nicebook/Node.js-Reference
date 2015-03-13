bffe6bdcc20a06f07f04f4f24349f4520531f656
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:48:37 +0800

    Add the example to connect RabbitMG server.

---
+++ 17/amqp-basic.js
@@ -0,0 +1,14 @@
+﻿var amqp = require('amqp');
+
+var connection = amqp.createConnection({
+    host: 'localhost'
+});
+
+connection.on('ready', function () {
+    connection.queue('my-queue', function(q) {
+        q.bind('#');
+        q.subscribe(function(message) {
+            console.log(message);
+        });
+    });
+});
