a87d1a4c1997c138ffe334fa819c6952d2f4c4b0
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:53:15 +0800

    Add the example to create a requester via zmq.

---
+++ 17/zmq-requester.js
@@ -0,0 +1,15 @@
+﻿var zmq = require('zmq');
+
+var socket = zmq.socket('req');
+socket.bind('tcp://127.0.0.1:12345', function(err) {
+    if (err)
+        console.log(err);
+
+    socket.on('message', function(msg) {
+        console.log('got reply', msg.toString());
+    });
+
+    setInterval(function() {
+        socket.send('Hello');
+    }, 100);
+});
