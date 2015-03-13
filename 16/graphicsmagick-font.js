a35e491bc6d0d2aa7f315f301e6518034f69c509
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:37:08 +0800

    Add the example to write texts in image file via garphicsmagick.

---
+++ 16/graphicsmagick-font.js
@@ -0,0 +1,16 @@
+﻿var gm = require('gm');
+var dirpath = 'images/';
+var imgname = 'download-logo.png';
+var tmppath = 'tmp/';
+
+gm(dirpath + imgname) 
+
+    .font('fonts/ariali.ttf', 12) 
+    
+    .drawText(5, 10, 'Hello Magic.') 
+    
+    .write(tmppath + 'New_' + imgname, function (err) {
+        if (err)
+            throw err;
+        console.log('done');
+});
