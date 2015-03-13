b8a406915a7ebf2500ff257c67cddb3123abbeef
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:53:33 +0800

    Add the example to create a producer via zmq.

---
+++ 17/zmq-producer.js
@@ -0,0 +1,9 @@
+﻿var zmq = require('zmq');
+
+var producer = zmq.socket('push');
+
+producer.bindSync('tcp://localhost:3000');
+
+setInterval(function() {
+    producer.send('New Work!');
+}, 1000);
