51fbee911f16683e06a9848e7816fe86cf52d70b
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:32:53 +0800

    Add the example to resize image via ninja.

---
+++ 16/ninja-resize.js
@@ -0,0 +1,14 @@
+﻿var Image = require('image-ninja');
+
+var dirpath = 'images/';
+var imgname = 'download-logo.png';
+var tmppath = 'tmp/'
+		
+var image = new Image(dirpath + imgname);
+image
+	.crop(5, 10, 50, 100)
+	.resize(200, 400)
+	.save(tmppath + 'New_' + imgname)
+	.then(function (newImage) {
+		console.log('Done.'); 
+});	
