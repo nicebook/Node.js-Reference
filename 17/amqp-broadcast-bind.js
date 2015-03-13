cb0bea819f71afa4f01e8856e252c17aa177c1d9
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:52:04 +0800

    Add the example to subscribe the broadcast via amqp.

---
+++ 17/amqp-broadcast-bind.js
@@ -0,0 +1,18 @@
+﻿var amqp = require('amqp');
+
+var connection = amqp.createConnection({
+    host: 'localhost'
+});
+
+connection.on('ready', function () {
+    connection.exchange('broadcast', { type: 'fanout', autoDelete: false },
+        function(exchange) {
+        connection.queue('tmp-' + Math.random, { exclusive: true }, function(q) {
+            q.bind(exchange, '');
+            q.subscribe(function(message) {
+                console.log(message);
+            });
+
+        });
+    });
+});
