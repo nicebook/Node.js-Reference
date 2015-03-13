c12d4645964ae9fda0597dc83a2d8b7a7af41eb4
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:28:53 +0800

    Add the example to add the audio in the video file via ffmpeg.

---
+++ 16/ffmpeg-toFormat-audio.js
@@ -0,0 +1,21 @@
+﻿var ffmpeg = require('fluent-ffmpeg');
+
+var dirpath = 'video/';  
+var tmppath = 'tmp/'; 
+var audio = 'testaudio-one.wav';
+var videoname = 'output.avi'; 
+var Command = new ffmpeg({ source: tmppath + videoname }); 
+
+Command
+	.fromFormat('avi') 
+	.withVideoCodec('libx264') 
+	.withAudioCodec('libmp3lame') 
+	.addInput(dirpath + audio) 
+	.toFormat('mp4') 
+	.on('error', function(err) {
+		console.log('An error occurred: ' + err.message);
+	})
+	.on('end', function() { 
+		console.log('Processing finished !');
+	})
+	.saveToFile(tmppath + 'output_audio.mp4');    
