c1fd3b39c0c5962c0006a9a0ba6e5babb2354d48
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 11:12:28 +0800

    Add the example to use moment to use the array of the token format.

---
+++ 3/moment-token-array.js
@@ -0,0 +1,8 @@
+var moment = require('moment');
+
+moment("12-25-2014", ["MM-DD-YYYY", "YYYY-MM-DD"]); 
+moment("29-06-2014", ["MM-DD-YYYY", "DD-MM", "DD-MM-YYYY"]); 
+moment("05-06-2014", ["MM-DD-YYYY", "DD-MM-YYYY"]);    
+
+
+
