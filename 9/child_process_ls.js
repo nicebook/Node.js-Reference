3d550711e29ab3be257b65350afe583c39b7f1c7
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 12:20:36 +0800

    Add the example to list the folder with child process.

---
+++ 9/child_process_ls.js
@@ -0,0 +1,15 @@
+var child_process = require('child_process');
+
+var ls = child_process.spawn('ls', [ '-lh', '/usr' ]);
+
+ls.stdout.on('data', function (data) {
+    console.log('stdout: ' + data);
+});
+
+ls.stderr.on('data', function (data) {
+    console.log('stderr: ' + data);
+});
+
+ls.on('close', function (code) {
+    console.log('child process exited with code ' + code);
+});
