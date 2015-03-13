ddf2caf5106ed056b2fb4b9de930e55ac4797b62
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 11:00:24 +0800

    Add the example to set a time

---
+++ 3/date-settime.js
@@ -0,0 +1,7 @@
+var now = new Date();
+console.log(now.toString());
+var NewDate = new Date( new Date().getFullYear(),
+                    	                new Date().getMonth(),
+                    	                new Date().getDate(),
+                    	                10, 17, 35, 192);
+console.log(new Date(NewDate).toString());
