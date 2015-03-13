a818d7ac517b0013c8362f9cec31f2bc943f230a
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:53:46 +0800

    Add the example to create a consumer via zmq.

---
+++ 17/zmq-consumer.js
@@ -0,0 +1,8 @@
+﻿var zmq = require('zmq');
+
+var worker = zmq.socket('pull');
+worker.connect('tcp://localhost:3000');
+
+worker.on('message', function(data) {
+    console.log(data.toString());
+}, 1000);
