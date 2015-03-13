c387d50571702b9a7145ebea66541ee4159e2ee8
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:38:36 +0800

    Add the example to resize the image file via imagemagick.

---
+++ 16/imagemagick-resize.js
@@ -0,0 +1,13 @@
+﻿var gm = require('gm');
+var dirpath = 'images/';
+var imgname = 'download-logo.png';
+var tmppath = 'tmp/';
+var imageMagick = gm.subClass({ imageMagick: true });
+
+imageMagick(dirpath + imgname)  
+    .resize(300, 200) 
+    .write(tmppath + 'imageMagick_' + imgname, function (err) {
+        if (err)
+            throw err;
+        console.log('done');
+});  
