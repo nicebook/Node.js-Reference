b395f02cf30a64bd21af14bfd0883467962b4902
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:34:55 +0800

    Add the example to change format of image via sharp.

---
+++ 16/sharp-toFormat.js
@@ -0,0 +1,21 @@
+﻿var sharp = require('sharp');
+
+var dirpath = 'images/';
+var imgname = 'download-logo.png';
+var tmppath = 'tmp/'
+
+sharp(dirpath + imgname)
+    .jpeg() 
+    .toFile(tmppath + 'New_' + 'output.jpeg', function (err, info) {
+        if (err) 
+            throw err;
+        console.log('done');
+    });
+
+sharp(dirpath + imgname)
+    .webp() 
+    .toFile(tmppath + 'New_' + 'output.webp', function (err, info) {
+        if (err) 
+            throw err;
+        console.log('done');
+    });
