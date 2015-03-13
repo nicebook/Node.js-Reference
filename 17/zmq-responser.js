7683dea93d807035d0a7d0f9a1836ddcbb527508
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:53:01 +0800

    Add the example to create a responser via zmq.

---
+++ 17/zmq-responser.js
@@ -0,0 +1,11 @@
+﻿var zmq = require('zmq');
+
+var socket = zmq.socket('rep');
+socket.connect('tcp://127.0.0.1:12345');
+
+socket.on('message', function(msg) {
+    console.log('received request:', msg.toString());
+    setTimeout(function() {
+        socket.send('World');
+    }, 1000);
+});
