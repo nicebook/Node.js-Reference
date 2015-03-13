e4947669644c87562a25582a8273fa495913cfb1
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:42:06 +0800

    Add the example to draw a line via canvas.

---
+++ 16/canvas-lineTo.js
@@ -0,0 +1,11 @@
+﻿var Canvas = require('canvas');
+var fs = require('fs');
+
+var canvas = new Canvas(640, 480, 'svg');
+var context = canvas.getContext('2d');
+context.beginPath();
+context.moveTo(100, 100);
+context.lineTo(200, 200);
+context.strokeStyle = '#ff0000';
+context.stroke();
+fs.writeFile('output.svg', canvas.toBuffer());
