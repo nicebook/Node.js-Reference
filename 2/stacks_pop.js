ecd6a3850667a0384cb169bb3bed11a9e2cb084c
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 10:56:39 +0800

    Add the example to pop a element from an array.

---
+++ 2/stacks_pop.js
@@ -0,0 +1,13 @@
+var queue = [];
+
+queue.push('Fred');
+queue.push('Wesley');
+queue.push('Chuck');
+queue.push('Charles');
+queue.push('Frankie');
+
+var first  = queue.pop();
+console.log(first);
+
+var second = queue.pop();
+console.log(second);
