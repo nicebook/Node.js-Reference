ecfbee8fde8a16a4a5140de360ca04b6f652229b
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:40:22 +0800

    Add the example to change the image's format via imagemagick.

---
+++ 16/imagemagick-setFormat.js
@@ -0,0 +1,13 @@
+﻿var gm = require('gm');
+var dirpath = 'images/';
+var imgname = 'download-logo.png';
+var tmppath = 'tmp/';
+var imageMagick = gm.subClass({ imageMagick: true });
+
+imageMagick(dirpath + imgname)  
+    .setFormat('gif') 
+    .write(tmppath + 'imageMagick_' + imgname, function (err) {
+        if (err)
+            throw err;
+        console.log('done');
+});
