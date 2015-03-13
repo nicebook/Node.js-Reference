1fcd418f80e26ac2e19c354f696a18efb25c28e3
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:39:26 +0800

    Add the example to show image's info via garphicsmagick.

---
+++ 16/graphicsmagick-identify.js
@@ -0,0 +1,11 @@
+﻿var gm = require('gm');
+var dirpath = 'images/';
+var imgname = 'download-logo.png';
+
+gm(dirpath + imgname) 
+
+    .identify(function (err, data) { 
+        if (err) 
+            throw err;
+        console.log(data);
+    }); 
