771c5454580b6674461cfd54ee2dfb9ea41e03fd
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 11:01:51 +0800

    Add the example to use moment with basic usage with utc.

---
+++ 3/moment-basic_utc.js
@@ -0,0 +1,8 @@
+var moment = require('moment');
+ 
+console.log(moment().utc().second()); 
+console.log(moment().utc().seconds()); 
+console.log(moment().utc().second(12)); 
+console.log(moment().utc().seconds(12)); 
+console.log(moment().format()); 
+console.log(moment.utc().format()); 
