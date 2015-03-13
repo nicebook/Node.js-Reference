ed255b7f97d66de8423454def490e1d62435347c
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:43:07 +0800

    Add the example to draw a circle via canvas.

---
+++ 16/canvas-fill.js
@@ -0,0 +1,10 @@
+﻿var Canvas = require('canvas');
+var fs = require('fs');
+
+var canvas = new Canvas(640, 480, 'svg');
+var context = canvas.getContext('2d');
+context.beginPath();
+context.arc(100, 100, 50, 0, 2 * Math.PI, true);
+context.fillStyle = '#ff0000';
+context.fill();
+fs.writeFile('output.svg', canvas.toBuffer());
