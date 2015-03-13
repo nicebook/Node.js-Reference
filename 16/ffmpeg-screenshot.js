66af4fe9cd92e1a44815ee8bf2f2096236b14cc7
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:30:02 +0800

    Add the example to create screenshot from videio file via ffmpeg.

---
+++ 16/ffmpeg-screenshot.js
@@ -0,0 +1,22 @@
+﻿var ffmpeg = require('fluent-ffmpeg');
+
+var videoname = 'output.avi'; 
+var tmppath = 'tmp/'; 
+var Command = new ffmpeg({ source: tmppath + videoname }); 
+
+Command
+	.withSize('100%') 
+	.on('error', function(err) {
+		console.log('An error occurred: ' + err.message);
+	})
+	.on('end', function(filenames) {
+		console.log('Successfully generated ' + filenames.join(', '));
+	})
+	.takeScreenshots(
+		{
+			count: 3, 
+			timemarks: [ '0.5', '1', '50%' ], 
+			filename: 'thumbnail-%b-%i-%ss' 
+		},
+		tmppath 
+	);  
