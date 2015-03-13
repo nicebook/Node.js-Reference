f8b67604f9883c6c830312b54634a942e9471e68
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:32:19 +0800

    Add the example to adjust image's width/height via ninja.

---
+++ 16/ninja-basic.js
@@ -0,0 +1,15 @@
+﻿var Image = require('image-ninja');
+
+var dirpath = 'images/';
+var imgname = 'download-logo.png';
+var tmppath = 'tmp/'
+
+var image = new Image(dirpath + imgname); 
+
+image
+	.width(1000) 
+	.height(1000) 
+	.save(tmppath + imgname) 
+	.then(function (newImage) { 
+		console.log('Done.'); 
+	});
