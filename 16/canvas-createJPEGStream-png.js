ed7bda3f2e64d9d55e9b7bb2a5eef0f480ada318
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:44:03 +0800

    Add the example to save a png file via canvas.

---
+++ 16/canvas-createJPEGStream-png.js
@@ -0,0 +1,12 @@
+﻿var Canvas = require('canvas');
+var fs = require('fs');
+
+var canvas = new Canvas(640, 480, 'svg');
+var context = canvas.getContext('2d');
+context.beginPath();
+context.arc(100, 100, 50, 0, 2 * Math.PI, true);
+context.fillStyle = '#ff0000';
+context.fill();
+var output = fs.createWriteStream('output.png');
+var stream = canvas.createPNGStream();
+stream.pipe(output);
