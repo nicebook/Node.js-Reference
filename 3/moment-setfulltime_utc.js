80ad17a9ce1223e19fd0be5cbfa5bd901a40ecb8
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 11:07:02 +0800

    Add the example to use moment to set a full time with utc.

---
+++ 3/moment-setfulltime_utc.js
@@ -0,0 +1,5 @@
+var moment = require('moment');
+
+console.log(moment("2014-12-11 21+0800").format()); 
+console.log(moment("2014-W50-4 21+08:00").format()); 
+console.log(moment("2014-12-11 21Z").format()); 
