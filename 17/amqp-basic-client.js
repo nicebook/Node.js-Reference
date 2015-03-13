2eb79773ce89dbdb5bde3a0fd13bc34dde614700
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:49:18 +0800

    Add the example to send a message to RabbitMG server.

---
+++ 17/amqp-basic-client.js
@@ -0,0 +1,9 @@
+﻿var amqp = require('amqp');
+
+var connection = amqp.createConnection({
+    host: 'localhost'
+});
+
+connection.on('ready', function () {
+    connection.publish('my-queue', 'Hello');
+});
