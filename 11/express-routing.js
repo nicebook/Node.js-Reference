f437018d18bf9cd983fefcffe8aed094df6b4fee
WesleyTsai <wesleyboy42@gmail.com>
2015-03-13 14:13:04 +0800

    Add the example for simple routing by express.

---
+++ 11/express-routing.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send('Hello World!');
+});
+
+app.post('/', function(req, res) {
+    res.send('Hello World!');
+});
+
+app.listen(3000);
